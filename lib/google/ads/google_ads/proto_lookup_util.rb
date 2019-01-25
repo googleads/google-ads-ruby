19efc3eca89638f32892e8ea2ec93ef5a8c8ef2a
Javier Rosales <javier.rb_96@hotmail.com>
2019-01-25 15:07:09 +0100

    Fix dismiss recommendation operation (#17)
    * Fix dismiss recommendation

--- lib/google/ads/google_ads/proto_lookup_util.rb
+++ lib/google/ads/google_ads/proto_lookup_util.rb
@@ -684,6 +684,8 @@ module Google
           Customer: ['customer_service_pb', 'CustomerOperation'],
           ConversionAction: ['conversion_action_service_pb',
                'ConversionActionOperation'],
+          DismissRecommendation: ['recommendation_service_pb',
+              'DismissRecommendationOperation'],
           FeedItem: ['feed_item_service_pb', 'FeedItemOperation'],
           FeedMapping: ['feed_mapping_service_pb', 'FeedMappingOperation'],
           Feed: ['feed_service_pb', 'FeedOperation'],
