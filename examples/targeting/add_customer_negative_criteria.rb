6bcffe34b524e3e20c7637c86633ad24657ea7c3
Sam Phippen <samphippen@googlemail.com>
2019-03-13 13:55:49 -0400

    Add an example for negative customer criteria (#45)

---
+++ examples/targeting/add_customer_negative_criteria.rb
@@ -0,0 +1,106 @@
+#!/usr/bin/env ruby
+# Encoding: utf-8
+#
+# Copyright:: Copyright 2017, Google Inc. All Rights Reserved.
+#
+# License:: Licensed under the Apache License, Version 2.0 (the "License");
+#           you may not use this file except in compliance with the License.
+#           You may obtain a copy of the License at
+#
+#           http://www.apache.org/licenses/LICENSE-2.0
+#
+#           Unless required by applicable law or agreed to in writing, software
+#           distributed under the License is distributed on an "AS IS" BASIS,
+#           WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+#           implied.
+#           See the License for the specific language governing permissions and
+#           limitations under the License.
+#
+# This example adds various types of negative criteria to a customer. These
+# criteria will be applied to all campaigns for the customer.
+
+require 'optparse'
+require 'google/ads/google_ads'
+
+def add_customer_negative_criteria(customer_id)
+  # GoogleAdsClient will read a config file from
+  # ENV['HOME']/google_ads_config.rb when called without parameters
+  client = Google::Ads::GoogleAds::GoogleAdsClient.new
+
+  tragedy_criterion = client.resource(:CustomerNegativeCriterion)
+  tragedy_criterion.content_label = client.resource(:ContentLabelInfo)
+  tragedy_criterion.content_label.type = :TRAGEDY
+
+  placement_criterion = client.resource(:CustomerNegativeCriterion)
+  placement_criterion.placement = client.resource(:PlacementInfo)
+  placement_criterion.placement.url = client.wrapper.string("http://example.com")
+
+  ops = [
+    {create: tragedy_criterion},
+    {create: placement_criterion},
+  ]
+
+  customer_negative_criterion_service = client.service(
+    :CustomerNegativeCriterion
+  )
+
+  response = customer_negative_criterion_service.mutate_customer_negative_criteria(customer_id, ops)
+
+  # Display the results.
+  response.results.each do |negative_criterion|
+    puts "Customer negative criterion with criterion resource name #{negative_criterion.resource_name} was added"
+  end
+end
+
+if __FILE__ == $0
+  options = {}
+  # The following parameter(s) should be provided to run the example. You can
+  # either specify these by changing the INSERT_XXX_ID_HERE values below, or on
+  # the command line.
+  #
+  # Parameters passed on the command line will override any parameters set in
+  # code.
+  #
+  # Running the example with -h will print the command line usage.
+  options[:customer_id] = 'INSERT_CUSTOMER_ID_HERE'
+
+  OptionParser.new do |opts|
+    opts.banner = sprintf('Usage: add_customer_negative_criteria.rb [options]')
+
+    opts.separator ''
+    opts.separator 'Options:'
+
+    opts.on('-C', '--customer-id CUSTOMER-ID', String, 'Customer ID') do |v|
+      options[:customer_id] = v
+    end
+
+    opts.separator ''
+    opts.separator 'Help:'
+
+    opts.on_tail('-h', '--help', 'Show this message') do
+      puts opts
+      exit
+    end
+  end.parse!
+
+  begin
+    add_customer_negative_criteria(options.fetch(:customer_id))
+  rescue Google::Ads::GoogleAds::Errors::GoogleAdsError => e
+    e.failure.errors.each do |error|
+      STDERR.printf("Error with message: %s\n", error.message)
+      if error.location
+        error.location.field_path_elements.each do |field_path_element|
+          STDERR.printf("\tOn field: %s\n", field_path_element.field_name)
+        end
+      end
+      error.error_code.to_h.each do |k, v|
+        next if v == :UNSPECIFIED
+        STDERR.printf("\tType: %s\n\tCode: %s\n", k, v)
+      end
+    end
+  rescue Google::Gax::RetryError => e
+    STDERR.printf("Error: '%s'\n\tCause: '%s'\n\tCode: %d\n\tDetails: '%s'\n" \
+        "\tRequest-Id: '%s'\n", e.message, e.cause.message, e.cause.code,
+                  e.cause.details, e.cause.metadata['request-id'])
+  end
+end
