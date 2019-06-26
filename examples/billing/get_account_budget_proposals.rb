f56a3e533cbe43c0f9fd833e599c18b15fa83c2c
Sam Phippen <samphippen@googlemail.com>
2019-06-26 09:58:44 -0400

    port examples/billing/get_account_budget_proposals.rb

--- examples/billing/get_account_budget_proposals.rb
+++ examples/billing/get_account_budget_proposals.rb
@@ -27,7 +27,7 @@ def get_account_budget_proposals(customer_id)
   # ENV['HOME']/google_ads_config.rb when called without parameters
   client = Google::Ads::GoogleAds::GoogleAdsClient.new
 
-  ga_service = client.service(:GoogleAds)
+  ga_service = client.service.google_ads
 
   search_query = <<~QUERY
     SELECT
