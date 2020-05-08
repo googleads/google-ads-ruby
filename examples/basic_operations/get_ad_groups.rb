e3cb0eddb453fb247f4095488cd9758bf5fa93e1
Fei Xiang <xflesym@users.noreply.github.com>
2020-05-08 14:39:07 +0800

    minor fix for get_ad_groups

--- examples/basic_operations/get_ad_groups.rb
+++ examples/basic_operations/get_ad_groups.rb
@@ -60,7 +60,7 @@ if __FILE__ == $0
     opts.separator ''
     opts.separator 'Options:'
 
-    opts.on('-C', '--customerid CUSTOMER-ID', String, 'Customer ID') do |v|
+    opts.on('-C', '--customer-id CUSTOMER-ID', String, 'Customer ID') do |v|
       options[:customer_id] = v
     end
 
