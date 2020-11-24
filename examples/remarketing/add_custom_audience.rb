187cf6e0ea8828d87f519119a23c0d64d3d9e4f4
Fei Xiang <xflesym@users.noreply.github.com>
2020-11-24 15:51:55 +0800

    code example for add_custom_audience

---
+++ examples/remarketing/add_custom_audience.rb
@@ -0,0 +1,136 @@
+#!/usr/bin/env ruby
+# Encoding: utf-8
+#
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# This example uses Customer Match to create a new user list (a.k.a. audience)
+# and adds users to it.
+#
+# This example illustrates adding a custom audience. Custom audiences help you
+# reach your ideal audience by entering relevant keywords, URLs and apps.
+# For more information about custom audiences, see:
+# https://support.google.com/google-ads/answer/9805516.
+
+require 'optparse'
+require 'google/ads/google_ads'
+require 'date'
+require 'digest'
+
+def add_custom_audience(customer_id)
+  # GoogleAdsClient will read a config file from
+  # ENV['HOME']/google_ads_config.rb when called without parameters
+  client = Google::Ads::GoogleAds::GoogleAdsClient.new
+
+  # Creates a custom audience operation.
+  operation = client.operation.create_resource.custom_audience do |ca|
+    ca.name = "Example Custom Audience ##{(Time.new.to_f * 1000).to_i}"
+    ca.description = "Custom audiences who have searched specific terms on Google Search"
+    # Match customers by what they searched on Google Search.
+    # Note: "INTEREST" OR "PURCHASE_INTENT" is not allowed for the type field
+    # of newly created custom audience. Use "AUTO" instead of these 2 options
+    # when creating a new custom audience.
+    ca.type = :SEARCH
+    ca.status = :ENABLED
+    # List of members that this custom audience is composed of. Customers that
+    # meet any of the membership conditions will be reached.
+    ca.members += [
+      # Keywords or keyword phrases, which describe the customers' interests
+      # or search terms.
+      create_custom_audience_member(client, :KEYWORD, "Mars Cruise"),
+      create_custom_audience_member(client, :KEYWORD, "Jupiter Cruise"),
+      # Website URLs that your customers might visit.
+      create_custom_audience_member(client, :URL, "http://www.example.com/locations/mars"),
+      create_custom_audience_member(client, :URL, "http://www.example.com/locations/jupiter"),
+      # Package names of Android apps which customers might install.
+      create_custom_audience_member(client, :APP, "com.google.android.apps.adwords"),
+    ]
+  end
+
+  # Issues a mutate request to add the custom audience.
+  response = client.service.custom_audience.mutate_custom_audiences(
+    customer_id: customer_id,
+    operations: [operation],
+  )
+  puts "New custom audience added with resource name: " \
+    "'#{response.results.first.resource_name}'."
+end
+
+# Creates a custom audience member.
+def create_custom_audience_member(client, member_type, member_value)
+  client.resource.custom_audience_member do |m|
+    m.member_type = member_type
+    case member_type
+    when :KEYWORD
+      m.keyword = member_value
+    when :URL
+      m.url = member_value
+    when :APP
+      m.app = member_value
+    else
+      raise "Invalid audience member type."
+    end
+  end
+end
+
+if __FILE__ == $0
+  options = {}
+  # The following parameter(s) should be provided to run the example. You can
+  # either specify these by changing the INSERT_XXX_ID_HERE values below, or on
+  # the command line.
+  #
+  # Parameters passed on the command line will override any parameters set in
+  # code.
+  #
+  # Running the example with -h will print the command line usage.
+  options[:customer_id] = 'INSERT_CUSTOMER_ID_HERE'
+
+  OptionParser.new do |opts|
+    opts.banner = sprintf('Usage: %s [options]', File.basename(__FILE__))
+
+    opts.separator ''
+    opts.separator 'Options:'
+
+    opts.on('-C', '--customer-id CUSTOMER-ID', String, 'Customer ID') do |v|
+      options[:customer_id] = v
+    end
+
+    opts.separator ''
+    opts.separator 'Help:'
+
+    opts.on_tail('-h', '--help', 'Show this message') do
+      puts opts
+      exit
+    end
+  end.parse!
+
+  begin
+    add_custom_audience(options.fetch(:customer_id).tr("-", ""))
+  rescue Google::Ads::GoogleAds::Errors::GoogleAdsError => e
+    e.failure.errors.each do |error|
+      STDERR.printf("Error with message: %s\n", error.message)
+      if error.location
+        error.location.field_path_elements.each do |field_path_element|
+          STDERR.printf("\tOn field: %s\n", field_path_element.field_name)
+        end
+      end
+      error.error_code.to_h.each do |k, v|
+        next if v == :UNSPECIFIED
+        STDERR.printf("\tType: %s\n\tCode: %s\n", k, v)
+      end
+    end
+    raise
+  end
+end
+
