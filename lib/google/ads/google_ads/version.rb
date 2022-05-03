d76d9d472d93fa650d0109f16358f0d4ff2359bc
Michael Cloonan <mcloonan@google.com>
2022-05-03 14:05:02 +0000

    Version bump for release.
    Change-Id: I043cabfe8040bcd7594d3d0e22dea75af45e553b

--- lib/google/ads/google_ads/version.rb
+++ lib/google/ads/google_ads/version.rb
@@ -20,7 +20,7 @@ module Google
   module Ads
     module GoogleAds
       CLIENT_LIB_NAME = 'gccl'.freeze
-      CLIENT_LIB_VERSION = '16.0.0'.freeze
+      CLIENT_LIB_VERSION = '17.0.0'.freeze
       VERSION = CLIENT_LIB_VERSION
     end
   end
