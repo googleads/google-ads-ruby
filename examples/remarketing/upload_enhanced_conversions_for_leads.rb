8901b1be411ba3498a7e878ab656984533a9c77c
Daniel Rodriguez <daniel@danielfrg.com>
2023-10-20 15:33:43 -0500

    V15: Rename and update enhanced conversions examples (#462)

--- examples/remarketing/upload_enhanced_conversions_for_leads.rb
+++ examples/remarketing/upload_enhanced_conversions_for_leads.rb
@@ -15,25 +15,66 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-# Uploads a conversion using hashed email address instead of GCLID.
+# Uploads an enhanced conversion for leads by uploading a ClickConversion.
+# The click conversion has hashed, first-party user-provided data from your
+# website lead forms. This includes user identifiers, and optionally, a click ID
+# and order ID. With this information, Google can tie the conversion to the ad
+# that drove the lead.
 
 require 'optparse'
 require 'google/ads/google_ads'
 
-# [START upload_conversion_with_identifiers]
 def upload_conversion_with_identifiers(
   customer_id,
   conversion_action_id,
-  email_address,
   conversion_date_time,
   conversion_value,
-  order_id)
+  order_id,
+  gclid,
+  ad_user_data_consent)
   # GoogleAdsClient will read a config file from
   # ENV['HOME']/google_ads_config.rb when called without parameters
   client = Google::Ads::GoogleAds::GoogleAdsClient.new
 
-  # [START create_conversion]
+  # [START add_user_identifiers]
+  # Extract user email and phone from the raw data, normalize and hash it,
+  # then wrap it in UserIdentifier objects. Create a separate UserIdentifier
+  # object for each. The data in this example is hardcoded, but in your
+  # application you might read the raw data from an input file.
+
+  # IMPORTANT: Since the identifier attribute of UserIdentifier
+  # (https://developers.google.com/google-ads/api/reference/rpc/latest/UserIdentifier)
+  # is a oneof
+  # (https://protobuf.dev/programming-guides/proto3/#oneof-features), you must
+  # set only ONE of hashed_email, hashed_phone_number, mobile_id,
+  # third_party_user_id, or address_info. Setting more than one of these
+  # attributes on the same UserIdentifier will clear all the other members of
+  # the oneof. For example, the following code is INCORRECT and will result in
+  # a UserIdentifier with ONLY a hashed_phone_number:
+  #
+  # incorrectly_populated_user_identifier.hashed_email = "...""
+  # incorrectly_populated_user_identifier.hashed_phone_number = "...""
+
+  raw_record = {
+    # Email address that includes a period (.) before the Gmail domain.
+    "email" => "alex.2@example.com",
+    # Phone number to be converted to E.164 format, with a leading '+' as
+    # required.
+    "phone" => "+1 800 5550102",
+    # This example lets you input conversion details as arguments,
+    # but in reality you might store this data alongside other user data,
+    # so we include it in this sample user record.
+    "order_id" => order_id,
+    "gclid" => gclid,
+    "conversion_action_id" => conversion_action_id,
+    "conversion_date_time" => conversion_date_time,
+    "conversion_value" => conversion_value,
+    "currency_code" => "USD",
+    "ad_user_data_consent" => ad_user_data_consent,
+  }
+
   click_conversion = client.resource.click_conversion do |cc|
+    # [START add_conversion_details]
     cc.conversion_action = client.path.conversion_action(customer_id, conversion_action_id)
     cc.conversion_date_time = conversion_date_time
     cc.conversion_value = conversion_value.to_f
@@ -43,16 +84,38 @@ def upload_conversion_with_identifiers(
       cc.order_id = order_id
     end
 
+    unless raw_record["gclid"].nil?
+      cc.gclid = gclid
+    end
+
+    # Specifies whether user consent was obtained for the data you are
+    # uploading. For more details, see:
+    # https://www.google.com/about/company/user-consent-policy
+    unless raw_record["ad_user_data_consent"].nil?
+      # cc.consent.ad_user_data = client.enums.ConsentStatusEnum[
+      #   raw_record["ad_user_data_consent"]
+      # ]
+    end
+    # [END add_conversion_details]
+
     # Creates a user identifier using the hashed email address, using the
     # normalize and hash method specifically for email addresses.
     # If using a phone number, use the normalize_and_hash method instead.
-    cc.user_identifiers << client.resource.user_identifier do |id|
-      id.hashed_email = normalize_and_hash_email(email_address)
+    cc.user_identifiers << client.resource.user_identifier do |ui|
+      ui.hashed_phone_number = normalize_and_hash_email(raw_record["email"])
       # Optional: Specifies the user identifier source.
-      id.user_identifier_source = :FIRST_PARTY
+      ui.user_identifier_source = :FIRST_PARTY
+    end
+
+    # Checks if the record has a phone number, and if so, adds a UserIdentifier
+    # for it.
+    unless raw_record["phone"].nil?
+      cc.user_identifiers << client.resource.user_identifier do |ui|
+        ui.hashed_phone_number = normalize_and_hash_email(raw_record["phone"])
+      end
     end
   end
-  # [END create_conversion]
+  # [END add_user_identifiers]
 
   response = client.service.conversion_upload.upload_click_conversions(
     customer_id: customer_id,
@@ -69,7 +132,6 @@ def upload_conversion_with_identifiers(
       "to #{result.conversion_action}."
   end
 end
-# [END upload_conversion_with_identifiers]
 
 # [START normalize_and_hash]
 # Returns the result of normalizing and then hashing the string using the
@@ -111,6 +173,8 @@ if __FILE__ == $0
   options[:conversion_date_time] = 'INSERT_CONVERSION_DATE_TIME_HERE'
   options[:conversion_value] = 'INSERT_CONVERSION_VALUE_HERE'
   options[:order_id] = nil
+  options[:gclid] = 'INSERT_GCLID_HERE'
+  options[:ad_user_data_consent] = 'INSERT_AD_USER_DATA_CONSENT_ENUM_HERE'
 
   OptionParser.new do |opts|
     opts.banner = sprintf('Usage: %s [options]', File.basename(__FILE__))
@@ -124,10 +188,6 @@ if __FILE__ == $0
 
     opts.on('-c', '--conversion-action-id CONVERSION-ACTION-ID', String, 'Conversion Action ID') do |v|
       options[:conversion_action_id] = v
-    end
-
-    opts.on('-e', '--email-address EMAIL-ADDRESS', String, 'Email address') do |v|
-      options[:email_address] = v
     end
 
     opts.on('-t', '--conversion-date-time CONVERSION-DATE-TIME', String,
@@ -145,6 +205,16 @@ if __FILE__ == $0
       options[:order_id] = v
     end
 
+    opts.on('-g', '--gclid GCLID', String, 'The Google click ID (gclid) for the click') do |v|
+      options[:gclid] = v
+    end
+
+    opts.on('-d', '--ad-user-data-dconsent GCLID', String,
+            'The data consent status for ad user data for all members in' \
+            'the job.') do |v|
+      options[:ad_user_data_consent] = v
+    end
+
     opts.separator ''
     opts.separator 'Help:'
 
@@ -158,10 +228,11 @@ if __FILE__ == $0
     upload_conversion_with_identifiers(
       options.fetch(:customer_id).tr("-", ""),
       options.fetch(:conversion_action_id),
-      options.fetch(:email_address),
       options.fetch(:conversion_date_time),
       options.fetch(:conversion_value),
       options[:order_id],
+      options[:gclid],
+      options[:ad_user_data_consent],
     )
   rescue Google::Ads::GoogleAds::Errors::GoogleAdsError => e
     e.failure.errors.each do |error|
