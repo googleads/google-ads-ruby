2a6d59f114334267e22f4469ade27512299ee67c
dorasun <dorasun@google.com>
2026-07-07 11:34:52 +0000

    Add incentives examples
    Change-Id: I57fa8a776fb8a1cbd43f6c1d3a1a71c3c63bdd19

---
+++ examples/incentives/fetch_incentive.rb
@@ -0,0 +1,155 @@
+# frozen_string_literal: true
+
+# Copyright 2026 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# This example fetches the available incentives for a user.
+
+require 'google/ads/google_ads'
+require 'optparse'
+
+def fetch_incentive(email, language_code, country_code)
+  # GoogleAdsClient will read a config file from a default location
+  # if no path is passed.
+  client = Google::Ads::GoogleAds::GoogleAdsClient.new
+
+  # Issues the request.
+  response = client.service.incentive.fetch_incentive(
+    email: email,
+    language_code: language_code,
+    country_code: country_code,
+    # IncentiveType is usually an enum, we can refer to it via its symbol representation
+    # or the exact class if needed. Let's pass the symbol :ACQUISITION.
+    type: :ACQUISITION
+  )
+
+  # Processes the response.
+  if response.incentive_offer.nil?
+    puts "No incentive offer was found"
+    return
+  end
+
+  # If the offer type is CHOOSE_YOUR_OWN_INCENTIVE, there will be 3 incentives in the
+  # response. At the time this example was written, all incentive offers are CYO incentive offers.
+  if response.incentive_offer.cyo_incentives
+    cyo_incentives = response.incentive_offer.cyo_incentives
+    print_incentive_details(cyo_incentives.low_offer)
+    print_incentive_details(cyo_incentives.medium_offer)
+    print_incentive_details(cyo_incentives.high_offer)
+  end
+rescue Google::Ads::GoogleAds::Errors::GoogleAdsError => e
+  puts "Request ID #{e.request_id} failed due to GoogleAdsError."
+  e.failure.errors.each_with_index do |error, i|
+    puts "  Error #{i}: #{error.message}"
+  end
+  exit 1
+end
+
+def print_incentive_details(incentive)
+  return if incentive.nil?
+
+  puts "===================================================================="
+  puts "Incentive ID: '#{incentive.incentive_id}'"
+  puts "Incentive requirement: '#{format_requirement(incentive.requirement)}'"
+  puts "Incentive terms and conditions: '#{incentive.incentive_terms_and_conditions_url}'"
+  puts "===================================================================="
+end
+
+def format_requirement(requirement)
+  return 'No requirements' if requirement.nil?
+
+  if requirement.spend
+    spend = requirement.spend
+    required = format_money(spend.required_amount)
+    award = format_money(spend.award_amount)
+    "Spend #{required} to receive #{award}"
+  else
+    requirement.to_s
+  end
+end
+
+def format_money(money)
+  return 'N/A' if money.nil?
+
+  amount = money.units
+  amount += money.nanos / 1_000_000_000.0 if money.nanos > 0
+
+  formatted_amount = money.nanos > 0 ? sprintf("%.2f", amount) : amount.to_s
+  "#{formatted_amount} #{money.currency_code}"
+end
+
+if __FILE__ == $0
+  options = {}
+  # The following parameter(s) should be provided to run the example. You can
+  # either specify these by changing the default values below, or on
+  # the command line.
+  #
+  # Parameters passed on the command line will override any parameters set in
+  # code.
+  #
+  # Running the example with -h will print the command line usage.
+  options[:language_code] = 'en'
+  options[:country_code] = 'US'
+
+  OptionParser.new do |opts|
+    opts.banner = sprintf('Usage: %s [options]', File.basename(__FILE__))
+
+    opts.separator ''
+    opts.separator 'Options:'
+
+    opts.on('-E', '--email EMAIL', String, 'Email') do |v|
+      options[:email] = v
+    end
+
+    opts.on('-L', '--language-code LANGUAGE-CODE', String, 'Language Code') do |v|
+      options[:language_code] = v
+    end
+
+    opts.on('-K', '--country-code COUNTRY-CODE', String, 'Country Code') do |v|
+      options[:country_code] = v
+    end
+
+    opts.separator ''
+    opts.separator 'Help:'
+
+    opts.on_tail('-h', '--help', 'Show this message') do
+      puts opts
+      exit
+    end
+  end.parse!
+
+  # Check if required parameters are present.
+  if options[:email].nil?
+    puts "Missing required argument: Email is required."
+    exit 1
+  end
+
+  begin
+    fetch_incentive(options[:email], options[:language_code], options[:country_code])
+  rescue Google::Ads::GoogleAds::Errors::GoogleAdsError => e
+    e.failure.errors.each do |error|
+      STDERR.printf("Error with message: %s\n", error.message)
+      if error.location
+        error.location.field_path_elements.each do |field_path_element|
+          STDERR.printf("\tOn field: %s\n", field_path_element.field_name)
+        end
+      end
+      error.error_code.to_h.each do |k, v|
+        next if v == :UNSPECIFIED
+        STDERR.printf("\tType: %s\n\tCode: %s\n", k, v)
+      end
+    end
+    raise
+  end
+end
